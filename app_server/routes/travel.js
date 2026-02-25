ac3a66555dc0a0455b890a4ed92472d769ad150c
Florinde <dcloud18@gmail.com>
2026-02-24 23:57:21 -0700

    Fix travel route controller import

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const ctrlTravel = require('../controllers/travel');
+const ctrlTravel = require('../controllers/travlr');
 
 /* GET travel page. */
 router.get('/', ctrlTravel.travel);
